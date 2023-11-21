114880418740243868930266521ad23bb58055c8
nguyencuong <tncuong0302@gmail.com>
2023-11-21 14:34:33 +0700

    Update in_app_webview.dart
    fixed initSurfaceAndroidView

--- lib/src/in_app_webview/in_app_webview.dart
+++ lib/src/in_app_webview/in_app_webview.dart
@@ -466,7 +466,7 @@ class _InAppWebViewState extends State<InAppWebView> {
     required Map<String, dynamic> creationParams,
   }) {
     if (hybridComposition) {
-      return PlatformViewsService.initAndroidView(
+      return PlatformViewsService.initSurfaceAndroidView(
         id: id,
         viewType: viewType,
         layoutDirection: layoutDirection,
